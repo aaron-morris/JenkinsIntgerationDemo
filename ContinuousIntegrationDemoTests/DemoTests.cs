fc4530c94cc6bf89745fe215796f824c11d3defb
Aaron Morris <amorris@tx3services.com>
2016-01-27 08:11:25 -0500

    Implement two sample unit tests: one always pass, one always fail

---
+++ ContinuousIntegrationDemoTests/DemoTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+
+namespace ContinuousIntegrationDemoTests
+{
+    using ContinousIntegrationDemo;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DemoTests
+    {
+        [TestCase]
+        public void DemoTest_TestPasses()
+        {
+            Assert.True(new Demo().Test());
+        }
+
+        [TestCase]
+        public void DemoTest_TestFails()
+        {
+            Assert.False(new Demo().Test());
+        }
+    }
+}
